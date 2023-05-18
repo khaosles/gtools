fd42c178bfeedce2c6bd4eead59fe11baa861cb2
khaosles <khaosles@163.com>
2023-05-18 23:52:04 +0800

    gt

---
+++ gt/consts.go
@@ -0,0 +1,23 @@
+package gt
+
+/*
+   @File: consts.go
+   @Author: khaosles
+   @Time: 2023/5/18 23:38
+   @Desc:
+*/
+
+const (
+	YYYY = "2006"
+	MMo  = "01"
+	DD   = "02"
+	H12  = "03"
+	H24  = "15"
+	MMi  = "04"
+	SS   = "05"
+)
+
+const (
+	YYYYMMDD       = "20060102"
+	YYYYMMDDHHMMSS = "20060102150405"
+)
